87e9cc1207af86ab0f9b2d1f347a296b4c4c70b4
Jihoon <jhc20@sfu.ca>
2012-07-02 20:25:29 -0700

    created comment function

---
+++ comment.js
@@ -0,0 +1,43 @@
+	{"user":"mkn3",
+	"upvote":"0",
+	"downvote":"0",
+	"description":"The answer to your question is...",
+	"questionIDs":"pJfznhheQuOicWWAjx7F00",
+	"timestamp":"2012-06-30"
+	}
+
+//Comment Model
+var Comment = function(user){
+	this.user = user;
+	this.upvote = 0;
+	this.downvote = 0;
+	this.description = "No Comment.";
+	this.id = "pJfznhheQuOicWWAjx7F00";
+	this.timestamp	= "2012-07-01";
+}
+
+Comment.prototype.getDescription = function(){
+	return this.description;
+}
+
+Comment.prototype.getID = function(){
+	return this.id;
+}
+
+Comment.prototype.setDescription = function(dsc){
+	this.description = dsc;
+}
+
+Comment.prototype.setID = function(pid){
+	this.id = pid;
+}
+
+Comment.prototype.vote = function(){
+	this.upvote = this.upvote + 1;
+}
+
+Comment.prototype.downvote = function(){
+	this.downvote = this.downvote + 1;
+}
+
+module.exports = Comment;
